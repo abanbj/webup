ef1c129357fac9923a1f5c76238d3142cb6ab150
andreas <panden@gmail.com>
2018-04-09 20:44:46 +0200

    Jobber videre.

--- webup.py
+++ webup.py
@@ -4,12 +4,17 @@ import urllib.request
 import hashlib
 #Add sqlite capability
 import sqlite3
+#Vi trenger også dato og klokkeslett
+import datetime
 
 #Sites som skal fingerprintes. MÅ ha med http(s):// ellers klager python
 sites = ['http://www.mareano.no/nyheter/nyheter-2018', 'http://www.npd.no']
 
-#Åpne connection til sqlite3
-sqlite_connection = sqlite3.connect('site_fingerprints.db')
+#Åpne connection til sqlite3. Her bør vi først sjekke om filen finnes!
+db_navn = "site_fingerprints.db"
+sqlite_connection = sqlite3.connect(db_navn)
+
+dato_og_tid = datetime.datetime.now()
 
 for url in sites:
  #Åpne URL
@@ -28,3 +33,4 @@ sqlite_connection.commit()
 
 #Lukk sqlite connection
 sqlite_connection.close()
+#Vi kan komponere og sende epost direkte fra Python?!
