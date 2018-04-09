743e1c84aba362eef8024fdd9d59736072ac5777
andreas <panden@gmail.com>
2018-04-09 10:34:57 +0200

    Endret litt.

--- webup.py
+++ webup.py
@@ -1,18 +1,17 @@
-###
 #Import bib for å laste ned ting
 import urllib.request
+#Import bib for å kalkulere hash
 import hashlib
 
-url = 'http://npd.no'
+#Velg URL som skal behandles
+url = 'http://www.mareano.no/nyheter/nyheter-2018'
+#Åpne URL
 response = urllib.request.urlopen(url)
+#Les inn innholdet fra URL inn i variabelen data
 data = response.read()      # a `bytes` object
 
-#m = hashlib.sha256(data)
-#m.update(data)
-file = open("hashtest.txt", "r") as fil
-print(hashlib.sha256(fil.read()).hexdigest())
-#print(m.hexdigest())
-
-#print(m.digest_size) #32
-#print(m.block_size) #64
-#print("H")
+#Beregn sha256 checksum og lagre som hex.
+#Denne ser ut som noe slik: 18e8606918705524af8f0c80b0ddb7cf0340038be8da79fccaa617b0c266f7ce
+m = hashlib.sha256(data).hexdigest()
+#Skriv ut checksumen
+print(m)
