494b64f687285dde1847820871065f354ac32f7b
andreas <panden@gmail.com>
2018-04-09 20:03:07 +0200

    Mer avansert. Tar nå en liste med nettsider som skal fingerprintes.

--- webup.py
+++ webup.py
@@ -2,16 +2,21 @@
 import urllib.request
 #Import bib for å kalkulere hash
 import hashlib
+#Add sqlite capability
+import sqlite3
 
-#Velg URL som skal behandles
-url = 'http://www.mareano.no/nyheter/nyheter-2018'
-#Åpne URL
-response = urllib.request.urlopen(url)
-#Les inn innholdet fra URL inn i variabelen data
-data = response.read()      # a `bytes` object
+#Sites som skal fingerprintes. MÅ ha med http(s):// ellers klager python
+sites = ['http://www.mareano.no/nyheter/nyheter-2018', 'http://www.npd.no']
 
-#Beregn sha256 checksum og lagre som hex.
-#Denne ser ut som noe slik: 18e8606918705524af8f0c80b0ddb7cf0340038be8da79fccaa617b0c266f7ce
-m = hashlib.sha256(data).hexdigest()
-#Skriv ut checksumen
-print(m)
+for url in sites:
+ #Åpne URL
+ response = urllib.request.urlopen(url)
+ #Les inn innholdet fra URL inn i variabelen data
+ data = response.read()      # a `bytes` object
+ 
+ #Beregn sha256 checksum og lagre som hex.
+ #Denne ser ut som noe slik: 18e8606918705524af8f0c80b0ddb7cf0340038be8da79fccaa617b0c266f7ce
+ m = hashlib.sha256(data).hexdigest()
+ #Skriv ut checksumen
+ print(m)
+  
