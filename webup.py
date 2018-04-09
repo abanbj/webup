3333628217042edc293f213f6dced0a85a930e60
andreas <panden@gmail.com>
2018-04-09 21:14:06 +0200

    Jobber videre.

--- webup.py
+++ webup.py
@@ -6,15 +6,24 @@ import hashlib
 import sqlite3
 #Vi trenger også dato og klokkeslett
 import datetime
+#For å sjekke om databasefilen eksisterer
+from pathlib import Path
+
 
 #Sites som skal fingerprintes. MÅ ha med http(s):// ellers klager python
 sites = ['http://www.mareano.no/nyheter/nyheter-2018', 'http://www.npd.no']
 
-#Åpne connection til sqlite3. Her bør vi først sjekke om filen finnes!
+#Åpne connection til sqlite3.
 db_navn = "site_fingerprints.db"
 sqlite_connection = sqlite3.connect(db_navn)
 
-dato_og_tid = datetime.datetime.now()
+dato_og_tid_naa = datetime.datetime.now()
+
+#Lag en curser slik at vi kan kjøre SQL-kommandoer
+c = sqlite_connection.cursor()
+
+#Kommenteres ut for nå. Her må vi sjekke om tabelle eksisterer fra før av!
+#c.execute('CREATE TABLE webside (date text, url text, hash text)')
 
 for url in sites:
  #Åpne URL
@@ -22,14 +31,18 @@ for url in sites:
  #Les inn innholdet fra URL inn i variabelen data
  data = response.read()      # a `bytes` object
  
- #Beregn sha256 checksum og lagre som hex.
- #Denne ser ut som noe slik: 18e8606918705524af8f0c80b0ddb7cf0340038be8da79fccaa617b0c266f7ce
+ #Beregn sha256 checksum
+ #Denne ser ut som noe slik: 18e8606918705524...
  m = hashlib.sha256(data).hexdigest()
- #Skriv ut checksumen
- print(m)
-  
-#Commit alle endringer
+ #Skriv ut dato og tid, url, og checksumen. Skriv også inn i databasen
+ print(dato_og_tid_naa, url, m)
+ c.execute("INSERT INTO webside VALUES ("dato_og_tid_naa","url","m")")
+
+#Commit alle endringer til databasen
 sqlite_connection.commit()
+
+for row in c.execute('SELECT * FROM webside'):
+	print(row)
 
 #Lukk sqlite connection
 sqlite_connection.close()
