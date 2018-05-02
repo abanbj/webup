e4e308bb81998740a336d7355a7676cbc8203f40
andreas <panden@gmail.com>
2018-05-02 14:24:02 +0200

    Bedre eposttekst.

--- webup.py
+++ webup.py
@@ -82,7 +82,7 @@ for url in sites:
    #textfile="test.txt"
    #with open(textfile) as fp:
    # Create a text/plain message
-   eposttekst = url + " har blitt oppdatert.\n\nNy hash er " + hash(url)
+   eposttekst = url + " har blitt oppdatert.\n\nNy/gammel hash er\n" + hash(url) + "\n" + old_hash[0][0]
    msg = MIMEText(eposttekst)
 
  
