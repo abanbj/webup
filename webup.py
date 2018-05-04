779fb5ba35297acfb558429bd3b4582b2f677774
andreas <panden@gmail.com>
2018-05-04 14:19:50 +0200

    Fiks indentering.

--- webup.py
+++ webup.py
@@ -10,10 +10,10 @@ import datetime
 import os
 
 def hash(url_hash):
-	response = urllib.request.urlopen(url_hash)
-	data = response.read()
-	m = hashlib.sha256(data).hexdigest()
-	return m
+    response = urllib.request.urlopen(url_hash)
+    data = response.read()
+    m = hashlib.sha256(data).hexdigest()
+    return m
 
 #Sites som skal fingerprintes. MÅ ha med http(s):// ellers klager python
 sites = [
@@ -42,80 +42,80 @@ c.execute('CREATE TABLE IF NOT EXISTS webside (date text, url text, hash text)')
 dato_og_tid_naa = datetime.datetime.now()
 
 for url in sites:
- #Finn ny hash til websiden. Denne må vi uansett ha
- new_hash = hash(url)
- #Finn gammel hash til siden
- sqlite_connection = sqlite3.connect(db_navn)
- c = sqlite_connection.cursor()
- query_oldhash = "SELECT hash FROM webside WHERE url='" + url + "' ORDER BY date DESC LIMIT 1" 
- old_hash = c.execute(query_oldhash).fetchall()
- #print(old_hash)
- sqlite_connection.commit()
- sqlite_connection.close()
+    #Finn ny hash til websiden. Denne må vi uansett ha
+    new_hash = hash(url)
+    #Finn gammel hash til siden
+    sqlite_connection = sqlite3.connect(db_navn)
+    c = sqlite_connection.cursor()
+    query_oldhash = "SELECT hash FROM webside WHERE url='" + url + "' ORDER BY date DESC LIMIT 1"
+    old_hash = c.execute(query_oldhash).fetchall()
+    #print(old_hash)
+    sqlite_connection.commit()
+    sqlite_connection.close()
 
- #Dersom gammel hash eksisterer så sjekker vi mot ny
- if old_hash:
-  sqlite_connection = sqlite3.connect(db_navn)
-  c = sqlite_connection.cursor()
-  print("Gammel hash for:", url, "funnet.")
-  print("Query for aa hente gammel hash er:", query_oldhash)
-  old_hash = c.execute(query_oldhash).fetchall()
-  sqlite_connection.commit()
-  sqlite_connection.close()
- 
-  print("Gammel hash er ", old_hash[0][0])
-  #print("Her er old hash ")
-  #Dette skal skje om hashen er ULIK
-  if not (old_hash[0][0] == new_hash):
- 
-   sqlite_connection = sqlite3.connect(db_navn)
-   c = sqlite_connection.cursor()
-   todo = [dato_og_tid_naa, url, new_hash]
-   c.execute("INSERT INTO webside VALUES (?,?,?)", todo)
-   print("La inn hash fra ", url)
-   print("Ny hash er   :" + new_hash)
-   sqlite_connection.commit()
-   sqlite_connection.close()
-   
-   #Send epost om at ny hash ble funnet
-   
-   # Import smtplib for the actual sending function
-   import smtplib
+    #Dersom gammel hash eksisterer så sjekker vi mot ny
+    if old_hash:
+        sqlite_connection = sqlite3.connect(db_navn)
+        c = sqlite_connection.cursor()
+        print("Gammel hash for:", url, "funnet.")
+        print("Query for aa hente gammel hash er:", query_oldhash)
+        old_hash = c.execute(query_oldhash).fetchall()
+        sqlite_connection.commit()
+        sqlite_connection.close()
 
-   # Import the email modules we'll need
-   from email.mime.text import MIMEText
+        print("Gammel hash er ", old_hash[0][0])
+        #print("Her er old hash ")
+        #Dette skal skje om hashen er ULIK
+        if not (old_hash[0][0] == new_hash):
 
-   # Open a plain text file for reading.  For this example, assume that
-   # the text file contains only ASCII characters.
-   #textfile="test.txt"
-   #with open(textfile) as fp:
-   # Create a text/plain message
-   eposttekst = url + " har blitt oppdatert.\n\nNy/gammel hash er\n" + new_hash + "\n" + old_hash[0][0]
-   msg = MIMEText(eposttekst)
+            sqlite_connection = sqlite3.connect(db_navn)
+            c = sqlite_connection.cursor()
+            todo = [dato_og_tid_naa, url, new_hash]
+            c.execute("INSERT INTO webside VALUES (?,?,?)", todo)
+            print("La inn hash fra ", url)
+            print("Ny hash er   :" + new_hash)
+            sqlite_connection.commit()
+            sqlite_connection.close()
 
- 
-   # me == the sender's email address
-   # you == the recipient's email address
-   msg['Subject'] = url + " er oppdatert"
-   msg['From'] = "server@mail.mail"
-   msg['To'] = "mail@mail.mail"
-   # Send the message via our own SMTP server.
-   s = smtplib.SMTP('localhost')
-   s.send_message(msg)
-   s.quit()
+            #Send epost om at ny hash ble funnet
+
+            # Import smtplib for the actual sending function
+            import smtplib
+
+            # Import the email modules we'll need
+            from email.mime.text import MIMEText
+
+            # Open a plain text file for reading.  For this example, assume that
+            # the text file contains only ASCII characters.
+            #textfile="test.txt"
+            #with open(textfile) as fp:
+            # Create a text/plain message
+            eposttekst = url + " har blitt oppdatert.\n\nNy/gammel hash er\n" + new_hash + "\n" + old_hash[0][0]
+            msg = MIMEText(eposttekst)
 
 
+            # me == the sender's email address
+            # you == the recipient's email address
+            msg['Subject'] = url + " er oppdatert"
+            msg['From'] = "server@mail.mail"
+            msg['To'] = "mail@mail.mail"
+            # Send the message via our own SMTP server.
+            s = smtplib.SMTP('localhost')
+            s.send_message(msg)
+            s.quit()
 
- else:
-  print("Gammel hash ikke funnet.")
-  sqlite_connection = sqlite3.connect(db_navn)
-  c = sqlite_connection.cursor()
-  todo = [dato_og_tid_naa, url, new_hash]
-  c.execute("INSERT INTO webside VALUES (?,?,?)", todo)
-  print("La inn hash fra ", url)
-  print("Ny hash er   :" + new_hash)
-  sqlite_connection.commit()
-  sqlite_connection.close()
+
+
+    else:
+        print("Gammel hash ikke funnet.")
+        sqlite_connection = sqlite3.connect(db_navn)
+        c = sqlite_connection.cursor()
+        todo = [dato_og_tid_naa, url, new_hash]
+        c.execute("INSERT INTO webside VALUES (?,?,?)", todo)
+        print("La inn hash fra ", url)
+        print("Ny hash er   :" + new_hash)
+        sqlite_connection.commit()
+        sqlite_connection.close()
 
 #Commit alle endringer til databasen
 #sqlite_connection.commit()
@@ -130,7 +130,7 @@ sqlite_connection = sqlite3.connect(db_navn)
 c = sqlite_connection.cursor()
 
 for row in c.execute('SELECT * FROM webside'):
-	print(row)
+    print(row)
 
 #Lukk sqlite connection
 sqlite_connection.close()
